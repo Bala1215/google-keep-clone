e980fedee5438668f14107b8de5df8ed4852ee0b
BalaChellammaV <121444048+BalaChellammaV@users.noreply.github.com>
2023-06-30 22:53:54 +0530

    Conditional rendering, useState, EventListener

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const CreateArea = () => {
+  
+  const [isExpanded,setIsExpanded] = useState(false);
+  //using update function of a state variable can only renders the component, there is any change occurs
+
+  //let expand = false;//variable , not a state variable
+
+  let expanded= ()=>{
+       setIsExpanded(true);
+       //expand=true;
+
+     return console.log(isExpanded);
+  };
+
   return (
     <div classname='createArea'>
         <form>
-            <input name='title' type='text' placeholder='Title'/>
-            <textarea name='content' placeholder='Take a note...' rows={3}></textarea>
+          {/* "conditional rendering" :
+                 when condition is satisfied , then it display that component or tag (in below eg)*/} 
+          {isExpanded && <input name='title' type='text' placeholder='Title'/> }  
+            <textarea name='content' placeholder='Take a note...' rows={3} onClick={expanded}></textarea>
         </form>
     </div>
   )
