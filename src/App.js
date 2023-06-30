67392d9d02b2c542deab6db92a85f70531b21f98
BalaChellammaV <121444048+BalaChellammaV@users.noreply.github.com>
2023-07-01 01:01:19 +0530

    functionality completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
+import React,{useState} from 'react';
 import './App.css';
 import CreateArea from './components/CreateArea';
 import Header from './components/Header';
+import Note from './components/Note'
 
 function App() {
+  const [notes,setNotes] = useState([]);
+
+  //get argument from CreateArea component 
+  function addNote(newNote){
+    //add new note into the array "notes" after it's previous values
+      setNotes((prevNotes)=>{
+        return[
+          ...prevNotes,
+          newNote
+        ]
+      });
+  }
+
+  console.log(notes);
+
+  function deleteNote(id){
+    setNotes((prevNotes)=>{
+      return prevNotes.filter((noteItem,index)=>{
+        return index!==id;
+      });
+    });
+  }
+
   return (
     <div className="App">
       <Header/>
-      <CreateArea/>
-
+      <CreateArea onAdd={addNote}/>
+      {
+        notes.map((noteItem,index)=>{
+          console.log(noteItem);
+          return <Note key={index} 
+                       title={noteItem.title} 
+                       content={noteItem.content}
+                       id={index}
+                       onDelete={deleteNote} 
+                       />;
+        })
+      }
     </div>
   );
 }
